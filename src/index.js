7680a3edfb3b37499bd77e655036b77cc7756550
rsal86 <96974191+rsal86@users.noreply.github.com>
2022-02-07 00:05:33 -0800

    js and html update
    removed forecast html and added it to the js.

--- src/index.js
+++ src/index.js
@@ -22,6 +22,32 @@ function formatDate(timestamp) {
 	return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+	let forecastElement = document.querySelector("#forecast");
+
+	let forecastHTML = `<div class = "row">`;
+	let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+	days.forEach(function (day) {
+		forecastHTML =
+			forecastHTML +
+			`<div class="col-2">
+						<div class="weather-forecast-date">${day}</div>
+						<img
+							src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+							alt=""
+							width="42"
+						/>
+						<div class="weather-forecast-temperatures">
+							<span class="weather-forecast-temperature-max">18° </span>
+							<span class="weather-forecast-temperature-min">12°</sßpan>
+						</div>
+					</div>`;
+	});
+
+	forecastHTML = forecastHTML + `</div>`;
+	forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
 	let temperatureElement = document.querySelector("#temp");
 	let cityElement = document.querySelector("#city");
@@ -105,3 +131,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
